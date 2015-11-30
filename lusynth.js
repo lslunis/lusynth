a337827942e380337910d00426acf336924d1ee2
Luke Stebbing <ls@lunis.net>
2015-11-30 10:05:28 -0500

    Record keystrokes using a simple .key polyfill
    This doesn't handle certain modifier key combinations, e.g. Ctrl-A.

--- lusynth.js
+++ lusynth.js
@@ -17,13 +17,12 @@ let isInt = Number.isSafeInteger
 let fix = (x, i) =>
     Math.round(x * i)
 
-let now = () =>
-    fix(performance.now(), 200)
-
 let draw = term =>
     isInt(term) ? new Text(term) : error()
 
 let world = 0
+
+let input = []
 
 let drawFrame = t => {
     let frame = document.body
@@ -35,3 +34,35 @@ let requestFrame = () =>
     requestAnimationFrame(drawFrame)
 
 requestFrame()
+
+let pushInput = (e, key) =>
+    input.push({
+        altKey: e.altKey,
+        ctrlKey: e.ctrlKey,
+        key,
+        metaKey: e.metaKey,
+        location: e.location,
+        repeat: e.repeat,
+        shiftKey: e.shiftKey,
+        timeStamp: e.timeStamp,
+    })
+
+addEventListener('keydown', e => {
+    let key = e.keyIdentifier
+    if (key.includes('+')) {
+        key = ({
+            'U+0008': 'Backspace',
+            'U+0009': 'Tab',
+            'U+0010': 'ContextMenu',
+            'U+001B': 'Escape',
+            'U+007F': 'Delete',
+        })[key]
+        if (!key) return
+    }
+    if (key == 'Backspace' || key == 'Tab') e.preventDefault()
+    pushInput(e, key)
+})
+
+addEventListener('keypress', e => {
+    if (e.which >= 32) pushInput(e, String.fromCharCode(e.which))
+})
