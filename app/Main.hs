9a291b843d39c34da0ff92bc961ad83a48f28cce
Luke Stebbing <ls@lunis.net>
2020-07-24 12:55:00 -0400

    add websocket support

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
+import Data.Text (Text)
+import Network.Wai.Handler.WebSockets (websocketsOr)
+import Network.Wai.Handler.Warp (defaultSettings, setPort, runSettings)
+import Network.WebSockets.Connection
+    ( sendTextData
+    , acceptRequest
+    , defaultConnectionOptions
+    , defaultPermessageDeflate
+    , connectionCompressionOptions
+    , CompressionOptions(PermessageDeflateCompression)
+    )
+import Network.Wai (responseLBS)
+import Network.HTTP.Types (status400)
 
-import Lib
+main = runSettings (setPort 1225 defaultSettings) app
 
-main :: IO ()
-main = someFunc
+app = websocketsOr
+    options
+    ((flip sendTextData ("hi" :: Text) =<<) . acceptRequest)
+    (const ($ responseLBS status400 [] "WebSocket required"))
+  where
+    options = defaultConnectionOptions
+        { connectionCompressionOptions = PermessageDeflateCompression
+            defaultPermessageDeflate
+        }
